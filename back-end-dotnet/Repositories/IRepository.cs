bf4bbdc5037b5d776dff3614a9e7488a5a3b3ba4
thaigiavuong <vuongtg@ctnpsolutions.com>
2024-10-26 23:13:11 +0700

    #9 - curd permission

---
+++ back-end-dotnet/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace back_end_dotnet;
+
+public interface IRepository<T>
+{
+    public Task<T> Create(T entity);
+    public Task<bool> Update(T entity);
+    public Task<List<T>> GetAll();
+    public Task<bool> Delete(T entity);
+    public Task<T> Get(int id);
+}
--- back-end-dotnet/Services/Impl/PermissionService.cs
+++ back-end-dotnet/Services/Impl/PermissionService.cs
@@ -1,6 +1,54 @@
-﻿namespace back_end_dotnet;
+﻿
+using AutoMapper;
+
+namespace back_end_dotnet;
 
 public class PermissionService : IPermissionService
 {
+    private readonly IPermissionRepository _permissionRepository;
+    private readonly IMapper _mapper;
+    public PermissionService(IPermissionRepository permissionRepository, IMapper mapper)
+    {
+        _permissionRepository = permissionRepository;
+        _mapper = mapper;
+    }
+    public async Task<PermissionResponse> CreatePermission(PermissionRequest permissionRequest)
+    {
+        PermissionEntity permissionEntity = _mapper.Map<PermissionEntity>(permissionRequest);
+        PermissionEntity permissionEntity1 = await _permissionRepository.Create(permissionEntity);
+        if (permissionEntity1 == null)
+        {
+            return null;
+        }
+        return _mapper.Map<PermissionResponse>(permissionEntity1);
+    }
 
+    public async Task<bool> DeletePermission(int id)
+    {
+        PermissionEntity permissionEntity = await _permissionRepository.Get(id);
+        return await _permissionRepository.Delete(permissionEntity);
+    }
+
+    public async Task<List<PermissionResponse>> GetPermissionEntities()
+    {
+        List<PermissionEntity> permissionEntities = await _permissionRepository.GetAll();
+
+        return _mapper.Map<List<PermissionResponse>>(permissionEntities); 
+    }
+
+    public async Task<PermissionResponse> GetPermissionEntity(int id)
+    {
+        PermissionEntity permissionEntity =  await _permissionRepository.Get(id);
+        if (permissionEntity == null){
+            return null;
+        }
+        return _mapper.Map<PermissionResponse>(permissionEntity);
+    }
+
+    public async Task<bool> UpdatePermission(int id, PermissionRequest permissionRequest)
+    {
+        PermissionEntity permission = await _permissionRepository.Get(id);
+        _mapper.Map(permission, permissionRequest);
+        return await _permissionRepository.Update(permission);
+    }
 }
