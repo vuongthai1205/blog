7414cb11bb701c9c08f75271a717d287d6c54af7
thaigiavuong <vuongtg@ctnpsolutions.com>
2024-11-15 22:30:56 +0700

    #7 - update handle auth with jwt

--- back-end-dotnet/DTO/Responses/UserResponse.cs
+++ back-end-dotnet/DTO/Responses/UserResponse.cs
@@ -4,6 +4,5 @@ public class UserResponse
 {
     public int UserId { get; set; }
     public string UserName { get; set; }
-    public string Password { get; set; }
     public List<RoleResponse>? Roles { get; set; }
 }
